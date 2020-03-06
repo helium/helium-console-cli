61e082b86c1467bde1e657726402b7585ca6561d
Louis Thiery <louis@helium.com>
2020-03-06 08:58:45 -0800

    add some README and comments

--- src/main.rs
+++ src/main.rs
@@ -15,23 +15,34 @@ use types::*;
 
 #[derive(StructOpt, Debug)]
 enum DeviceCmd {
+    /// List all your account devices
     List,
+    /// Get the full record of your device
+    /// by providing app_eui, app_key, and dev_eui
     Get {
         app_eui: String,
         app_key: String,
         dev_eui: String,
     },
+    /// Delete a device
+    /// by providing app_eui, app_key, and dev_eui
     Delete {
         app_eui: String,
         app_key: String,
         dev_eui: String,
     },
+    /// Get the full record of your device
+    /// by the UUID
     GetById {
         id: String,
     },
+    /// Delete a device
+    /// by the UUID
     DeleteById {
         id: String,
     },
+    /// Create a device
+    /// by providing app_eui, app_key, dev_eui and name
     Create {
         app_eui: String,
         app_key: String,
