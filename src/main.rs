f1e7e5ecacbf7b4e32d9a83d7f52b4948b40a1ab
Louis Thiery <louis@helium.com>
2020-03-11 11:07:50 -0700

    update device query

--- src/main.rs
+++ src/main.rs
@@ -15,72 +15,39 @@ mod client;
 mod config;
 mod ttn;
 mod types;
+mod clicmd;
 
 use config::get_input;
 use std::str::FromStr;
 use types::*;
+use clicmd::*;
 
-#[derive(StructOpt, Debug)]
-enum DeviceCmd {
-    /// List all your account devices
-    List,
-    /// Get the full record of your device
-    /// by providing app_eui, app_key, and dev_eui
-    Get {
-        app_eui: String,
-        app_key: String,
-        dev_eui: String,
-    },
-    /// Delete a device
-    /// by providing app_eui, app_key, and dev_eui
-    Delete {
-        app_eui: String,
-        app_key: String,
-        dev_eui: String,
-    },
-    /// Get the full record of your device
-    /// by the UUID
-    GetById { id: String },
-    /// Delete a device
-    /// by the UUID
-    DeleteById { id: String },
-    /// Create a device
-    /// by providing app_eui, app_key, dev_eui and name
-    Create {
-        app_eui: String,
-        app_key: String,
-        dev_eui: String,
-        name: String,
-    },
-}
-
-#[derive(StructOpt, Debug)]
-enum TtnCmd {
-    /// Imports devices from your TTN Account
-    /// (requires ttnctl access code at https://account.thethingsnetwork.org/)
-    Import,
-}
-
-/// Interact with Helium API via CLI
-#[derive(Debug, StructOpt)]
-enum Cli {
-    /// List, create, and delete devices
-    Device {
-        #[structopt(subcommand)]
-        cmd: DeviceCmd,
-    },
-    /// Import devices from TTN to Helium
-    Ttn {
-        #[structopt(subcommand)]
-        cmd: TtnCmd,
-    },
-}
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Config {
     key: String,
     base_url: String,
     request_timeout: u64,
+}
+
+/// Interact with Helium API via CLI
+#[derive(StructOpt, Debug)]
+pub enum Cli {
+    /// List, create, and delete devices
+    Device {
+        #[structopt(subcommand)]
+        cmd: DeviceCmd,
+    },
+    /// List, create, and delete labels
+    Device {
+        #[structopt(subcommand)]
+        cmd: LabelCmd,
+    },
+    /// Import devices from TTN to Helium
+    Ttn {
+        #[structopt(subcommand)]
+        cmd: TtnCmd,
+    },
 }
 
 #[tokio::main]
